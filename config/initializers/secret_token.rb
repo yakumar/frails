d9ebe0dd7ff9f90c835fc66a8a99f8975f5ee386
yakumar <yakumar_in@yahoo.com>
2014-03-20 16:47:35 +0530

    trying

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Frails::Application.config.secret_key_base = 'fef4d166fd3c7ecb97798e4d72e3ce912175a940e2144a01c0e587653c7dd98e8d7114c372dcc7bdc45fa4e2a7fb848bc682f1e15537d19e1502961f27b37709'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Frails::Application.config.session_store :cookie_store, key: '_frails_session'
